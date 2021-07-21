2b58c5c44b51c7861c5f7f6f873413ec3d5df633
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-21 00:40:01 -0300

    Declarações de variáveis

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 console.log();
 console.log("Aprendendo Javascript e executando o Node JS");
 console.log();
+
+
+//Trabalhando algumas regras no Javascript
+// Não podemos criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes("descrições significativas")
+
+let nomeCliente="Adriano";
+
+//Variáveis não podem iniciar com símbolos (exceto o underline __) ou números
+//Variáveis não podem conter espaços ou hífens (-)
+//Utilize os padrões camelCase, PascalCaso ou snakeCase
+
+//Declarações de variáveis são case sensitive
+//Não é possível redeclarar variáveis com o "let"
+//Atualmente é importante utilizar o LET no lugar de VAR
