1dace52087301b44371a70df91f15528feab19f4
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-21 10:40:22 -0300

    Operadores Aritmeticos

---
+++ exemplos/operadoresAritmeticos.js
@@ -0,0 +1,13 @@
+/* OPERADORES ARITM[ETICOS 
+    
+   - + ,  - ,  * , / ,  %  , ** 
+   
+   PRECED"ENCIA DE OPERADORES
+
+   1. Par:enteses ( )
+   2. Expon:encia;'ao **
+   3. Multiplica;'ao  *
+   4. Divis;ao e resto (/ e %)
+   5. Soma e subtra;'ao (+ -)
+    
+*/
