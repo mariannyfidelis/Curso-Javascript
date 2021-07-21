203c5aea40c118d87fc4cfcaafe8d70ef2a3fcaa
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-21 09:38:08 -0300

    Declaracao de variaveis

---
+++ exemplos/exe01.js
@@ -0,0 +1,15 @@
+//exercícios sobre CONST e LET
+
+const nome = "Marina Sousa";
+const sobrenome = "Sato";
+const idade=29;
+const peso=80;
+const alturaEmM=1.80;
+let imc;
+let anoNascimento;
+
+imc = peso/(alturaEmM*alturaEmM);
+anoNascimento = 2021-idade;
+console.log("O IMC de ", nome, " é ", imc, "e tem ", idade, " anos e nasceu em ", anoNascimento,);
+console.log("##");
+console.log(`O IMC de ${nome} é ${imc} tendo ${idade} anos e nasceu em ${anoNascimento}`);
---
+++ exemplos/exe02.js
@@ -0,0 +1,11 @@
+/* 
+
+- ECMASCRIPT [e a padroniza;'ao do Javascript
+- ES6 - ECMAscript 2015 - Javascript melhorado
+
+  Primeira diferenca entre VAR e LET 
+
+LET - n'ao permite redeclarar as vari[aveis
+  
+*/
+
