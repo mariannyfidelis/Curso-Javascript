b13622fb3896089804f4c51f8f6571525f8e8c06
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-21 15:27:20 -0300

    Operadores de comparação e Operadores Lógicos

---
+++ exemplos/operadoresComparacao.js
@@ -0,0 +1,21 @@
+/*   
+    OPERADORES DE COMPARACAO
+ >   MAIOR QUE
+ >=  MAIOR OU IGUAL A
+ <   MENOR QUE
+ <=  MENOR OU IGUAL A
+ ==  IGUALDADE (VALOR)
+ === IGUALDADE ESTRITA (VALOR E TIPO)
+ !=  DIFERENTE (VALOR)
+ !== DIFERENTE ESTRITO (VALOR E TIPO)
+
+ ESSAS COMPARACOES RETORNAM BOOLEANOS (VERDADEIRO OU FALSO)
+
+*/
+
+const num = 10;
+const num2 = '10';
+console.log(num == num2);
+
+//Maneira correta
+console.log(num ===num2);
---
+++ exemplos/operadoresLogicos.js
@@ -0,0 +1,21 @@
+/*
+    OPERADORES LÓGICOS
+
+    && -> AND -> E
+    || -> OR  -> OU
+    !  -> NOT -> NEGAÇÃO
+
+    - Permitem unir duas ou mais comparações 
+    - Trabalham em conjunto com os operadores de comparação
+*/
+console.log("== OPERADOR LOGICO AND ==");
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log("\n== OPERADOR LOGICO OR ==");
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
