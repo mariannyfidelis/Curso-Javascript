d9f957646859aef0b1be23bcc5b55c36c800ed10
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-21 09:38:43 -0300

    Conhecendo os Tipos de dados

---
+++ exemplos/tiposDeDados.js
@@ -0,0 +1,17 @@
+/*  
+    TIPOS DE DADOS PRIMITIVOS
+
+    -- STRING
+    -- NUMBER
+    -- BOOLEAN
+    -- UNDEFINED (N'AO APONTA PARA LOCAL NENHUM NA MEM[ORIA) 
+          - PROGRAMADOR N'AO PODE CONFIGURAR UNDEFINED S[O O JAVASCRIPT
+    -- NULL  (N'AO APONTA PARA LOCAL NENHUM NA MEM[ORIA) 
+        - PROGRAMADOR PODE CONFIGURAR NULL
+*/
+const nome = "C[ezar";
+const number1 = 23;
+const number2 = 2.2;
+const bool=true;
+const bool2=false;
+console.log(typeof nome, typeof number1, typeof bool, typeof bool2);
